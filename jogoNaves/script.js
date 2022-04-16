0277df1bc21312b6078fe9757c6210bc998178dd
Calvin <calvinsoares@17gmail.com>
2022-04-16 17:49:06 -0300

    arquivos necessarios adicionados

---
+++ jogoNaves/script.js
@@ -0,0 +1,11 @@
+function start() {   //funcao start utilizando o jquery
+
+    $("#inicio").hide();
+
+    $("#fundoGame").append("<div id='jogador'></div>");
+    $("#fundoGame").append("<div id='inimigo1'></div>");
+    $("#fundoGame").append("<div id='inimigo2'></div>");
+    $("#fundoGame").append("<div id='amigo'></div>");
+
+
+}
